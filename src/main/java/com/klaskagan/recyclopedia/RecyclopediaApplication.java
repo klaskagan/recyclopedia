a3acb694e9d31e397fb07dc142fbe870720b6924
Viktoras Baracevicius <klaskagan@gmail.com>
2020-04-04 21:56:06 +0300

    Initial commit

---
+++ src/main/java/com/klaskagan/recyclopedia/RecyclopediaApplication.java
@@ -0,0 +1,13 @@
+package com.klaskagan.recyclopedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecyclopediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecyclopediaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/klaskagan/recyclopedia/RecyclopediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klaskagan.recyclopedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecyclopediaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
